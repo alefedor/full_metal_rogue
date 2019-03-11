37181871a9a636c25b1cc036501817013d6bd38a
Alexandr Fedorov <fedorov2602@mail.ru>
2019-03-12 00:16:14 +0300

    Added MapInhabitator

---
+++ src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/map/MapInhabitator.kt
@@ -0,0 +1,56 @@
+package ru.hse.spb.sd.full_metal_rogue.map
+
+import ru.hse.spb.sd.full_metal_rogue.objects.Actor
+import ru.hse.spb.sd.full_metal_rogue.objects.FreeSpace
+import kotlin.random.Random
+
+interface MapInhabitator {
+    fun inhabitateWithActor(map: MutableGameMap, actor: Actor)
+}
+
+object SparseMapInhabitator : MapInhabitator {
+    override fun inhabitateWithActor(map: MutableGameMap, actor: Actor) {
+        val actorPositions = mutableListOf<Position>()
+        val freeSpacePositions = mutableListOf<Position>()
+
+        for (x in 0 until map.width)
+            for (y in 0 until map.height) {
+                if (map[x, y] == FreeSpace)
+                    freeSpacePositions.add(Position(x, y))
+                if (map[x, y] is Actor)
+                    actorPositions.add(Position(x, y))
+            }
+
+        val position = randomPosition(map, freeSpacePositions, actorPositions)
+
+        map[position.x, position.y] = actor
+    }
+
+    private fun randomPosition(map: MutableGameMap, freeSpacePositions: List<Position>, actorPositions: List<Position>): Position {
+        if (freeSpacePositions.isEmpty())
+            throw IllegalArgumentException("Map should have at least one free space")
+
+        val weights = DoubleArray(freeSpacePositions.size) { (map.width + map.height).toDouble() }
+
+        for (i in 0 until weights.size)
+            for (actorPosition in actorPositions)
+                weights[i] = Math.min(weights[i], freeSpacePositions[i].distanceTo(actorPosition).toDouble())
+
+        val totalWeight = weights.sum()
+
+        val rand = Random.nextDouble(totalWeight)
+
+        var sumWeight = 0.0
+
+        for (i in 0 until freeSpacePositions.size) {
+            sumWeight += weights[i]
+
+            if (sumWeight >= rand) {
+                return freeSpacePositions[i]
+            }
+        }
+
+        return freeSpacePositions.last()
+    }
+
+}
