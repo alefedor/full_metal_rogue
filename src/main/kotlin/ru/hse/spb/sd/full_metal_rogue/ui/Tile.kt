3bf85ce858df254c4ecef9c4f191e2a47ec078bc
sharkovadarya <sharkovadarya@gmail.com>
2019-03-13 15:24:09 +0300

    changed wall color

--- src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/ui/Tile.kt
+++ src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/ui/Tile.kt
@@ -5,6 +5,6 @@ import java.awt.Color
 
 enum class Tile(val glyph: Char, val color: Color) {
     FREE_SPACE('.', AsciiPanel.green),
-    WALL('#', AsciiPanel.brightMagenta),
+    WALL('#', AsciiPanel.magenta),
     BOUNDS('|', AsciiPanel.brightCyan)
 }
