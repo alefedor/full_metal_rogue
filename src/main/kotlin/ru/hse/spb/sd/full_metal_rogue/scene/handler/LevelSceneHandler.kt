2a27c9225158d9d816146b1c7dfd5351ba3c6a67
sharkovadarya <sharkovadarya@gmail.com>
2019-03-13 17:57:05 +0300

    Added player movement.

--- src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/scene/handler/LevelSceneHandler.kt
+++ src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/scene/handler/LevelSceneHandler.kt
@@ -1,6 +1,7 @@
 package ru.hse.spb.sd.full_metal_rogue.scene.handler
 
 import ru.hse.spb.sd.full_metal_rogue.map.MutableGameMap
+import ru.hse.spb.sd.full_metal_rogue.objects.*
 import ru.hse.spb.sd.full_metal_rogue.scene.LevelScene
 import ru.hse.spb.sd.full_metal_rogue.ui.SceneDrawer
 import java.awt.event.KeyEvent
@@ -22,13 +23,61 @@ class LevelSceneHandler(sceneDrawer: SceneDrawer, private val map: MutableGameMa
             else -> this
         }
 
-    private fun makeGameTurn(playerMove: PlayerMove): SceneHandler {
-        TODO()
+    private fun movePlayer(player: Player, playerMove: PlayerMove, oldX: Int, oldY: Int): Boolean {
+        val newX = oldX + playerMove.deltaX
+        val newY = oldY + playerMove.deltaY
+        if (isInMapBounds(newX, newY)) {
+            when (map[newX, newY]) {
+                is FreeSpace -> {
+                    map[oldX, oldY] = FreeSpace
+                    map[newX, newY] = player
+                    this.message = ""
+                    return true
+                }
+                is Wall -> this.message = "There is a wall in the way!"
+                is Enemy -> {
+                    // TODO handle mob confrontation
+                }
+            }
+        } else {
+            this.message = "There is a border in the way!"
+        }
+
+        return false
     }
 
+    private fun makeGameTurn(playerMove: PlayerMove): SceneHandler {
+        val movedActors = HashSet<Actor>()
+
+        for (i in 0 until map.height) {
+            for (j in 0 until map.width) {
+                if (map[j, i] is Actor && !movedActors.contains(map[j, i])) {
+                    when (map[j, i]) {
+                        is Player -> {
+                            val player: Player = map[j, i] as Player
+                            if (movePlayer(player, playerMove, j, i)) {
+                                movedActors.add(player)
+                            }
+                        }
+                        is Enemy -> {
+                            // TODO handle mob movement
+                            //movedActors.add(map[j, i] as Actor)
+                        }
+                    }
+                }
+            }
+        }
+
+        // TODO or should there be a new SceneHandler?
+        return this
+    }
+
+    private fun isInMapBounds(x: Int, y: Int): Boolean =
+        x >= 0 && x < map.width && y >= 0 && y < map.height
+
     private enum class PlayerMove(val deltaX: Int, val deltaY: Int) {
-        UP(0, 1),
-        DOWN(0, -1),
+        UP(0, -1),
+        DOWN(0, 1),
         LEFT(-1, 0),
         RIGHT(1, 0)
     }
