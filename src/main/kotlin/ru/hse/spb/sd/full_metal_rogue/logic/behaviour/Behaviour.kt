aa15ebcc695c5a58516f01219786ea33c498df1f
Alexandr Fedorov <fedorov2602@mail.ru>
2019-04-15 19:43:32 +0300

    Added behaviour

---
+++ src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/logic/behaviour/Behaviour.kt
@@ -0,0 +1,52 @@
+package ru.hse.spb.sd.full_metal_rogue.logic.behaviour
+
+import ru.hse.spb.sd.full_metal_rogue.logic.map.*
+
+interface Behaviour {
+    fun makeMove(currentPosition: Position, map: GameMap): Position
+}
+
+object PassiveBehaviour : Behaviour {
+    override fun makeMove(currentPosition: Position, map: GameMap): Position = currentPosition
+}
+
+object AgressiveBehaviour : Behaviour {
+    override fun makeMove(currentPosition: Position, map: GameMap): Position {
+        val distances = map.calculateDistancesFrom(map.playerPosition())
+        val possibleMoves = possibleMoves(currentPosition, map)
+        var intendedMove = possibleMoves[0]
+
+        for (move in possibleMoves)
+            if (distances[move.x][move.y] <= distances[intendedMove.x][intendedMove.y])
+                intendedMove = move
+
+        return intendedMove
+    }
+}
+
+object CowardBehaviour : Behaviour {
+    override fun makeMove(currentPosition: Position, map: GameMap): Position {
+        val distances = map.calculateDistancesFrom(map.playerPosition())
+        val possibleMoves = possibleMoves(currentPosition, map)
+        var intendedMove = possibleMoves[0]
+
+        for (move in possibleMoves)
+            if (distances[move.x][move.y] >= distances[intendedMove.x][intendedMove.y])
+                intendedMove = move
+
+        return intendedMove
+    }
+}
+
+fun possibleMoves(position: Position, map: GameMap): List<Position> {
+    val possibleMoves = mutableListOf(position)
+
+    for (direction in Direction.values()) {
+        val nextPosition = position.goToDirection(direction)
+
+        if (map.canPassThrough(nextPosition))
+            possibleMoves.add(nextPosition)
+    }
+
+    return possibleMoves
+}
