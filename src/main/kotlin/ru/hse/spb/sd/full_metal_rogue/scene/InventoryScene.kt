4af2a7b467465b09c29205502d98ea7a80c92c0d
Dmitry <qudeed@gmail.com>
2019-04-28 21:20:50 +0300

    Inventory scene

--- src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/scene/InventoryScene.kt
+++ src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/scene/InventoryScene.kt
@@ -3,5 +3,5 @@ package ru.hse.spb.sd.full_metal_rogue.scene
 import ru.hse.spb.sd.full_metal_rogue.logic.inventory.Item
 
 class InventoryScene(val inventoryItems: Menu<Item>,
-                     val equipedItems: Menu<Item>
+                     val equipedItems: List<Item>
 ) : Scene
