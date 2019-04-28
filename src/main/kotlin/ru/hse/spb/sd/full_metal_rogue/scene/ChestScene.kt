aa6f86ebd22e91325e8bb8b2d082c2b042a122d0
sharkovadarya <sharkovadarya@gmail.com>
2019-04-28 16:59:53 +0300

    Updated ChestScene and InventoryScene with properties; fixed errors in SceneDrawer.

--- src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/scene/ChestScene.kt
+++ src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/scene/ChestScene.kt
@@ -1,4 +1,5 @@
 package ru.hse.spb.sd.full_metal_rogue.scene
 
-class ChestScene : Scene {
-}
+import ru.hse.spb.sd.full_metal_rogue.logic.objects.Chest
+
+class ChestScene(val chest: Chest, val currentItem: Int) : Scene
