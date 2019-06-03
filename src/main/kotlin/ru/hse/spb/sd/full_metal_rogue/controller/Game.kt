d4d0e3f505437fae2ca93d146bb2873927ab3b04
Alexandr Fedorov <fedorov2602@mail.ru>
2019-06-03 16:03:50 +0300

    Game improvement

--- src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/controller/Game.kt
+++ src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/controller/Game.kt
@@ -2,12 +2,15 @@ package ru.hse.spb.sd.full_metal_rogue.controller
 
 import ru.hse.spb.sd.full_metal_rogue.logic.level.LevelGenerator
 import ru.hse.spb.sd.full_metal_rogue.logic.level.StandardLevelGenerator
+import ru.hse.spb.sd.full_metal_rogue.logic.map.MutableGameMap
+import ru.hse.spb.sd.full_metal_rogue.logic.objects.Player
 import ru.hse.spb.sd.full_metal_rogue.view.View
 import ru.hse.spb.sd.full_metal_rogue.view.handler.GameSceneHandler
 import ru.hse.spb.sd.full_metal_rogue.view.handler.LevelSceneHandler
+import java.lang.IllegalArgumentException
 import java.util.*
 
-class Game(levelGenerator: LevelGenerator = StandardLevelGenerator()) {
+class Game(map: MutableGameMap) {
     val view: View
         get() = handlersStack.peek().view
 
@@ -16,11 +19,20 @@ class Game(levelGenerator: LevelGenerator = StandardLevelGenerator()) {
     private val handlersStack = Stack<GameSceneHandler>()
 
     init {
-        handlersStack.push(LevelSceneHandler(levelGenerator.generateLevel()))
+        handlersStack.push(LevelSceneHandler(map))
+        for (x in 0 until map.width)
+            for (y in 0 until map.height) {
+                val gameObject = map.get(x, y)
+                if (gameObject is Player)
+                    playerList.add(gameObject.name)
+            }
     }
 
-    fun join(playerName: String) {
+    constructor(levelGenerator: LevelGenerator = StandardLevelGenerator()): this(levelGenerator.generateLevel()) {}
 
+    fun join(playerName: String) {
+        if (playerName in playerList)
+            throw IllegalArgumentException("Player with such name already exists")
     }
 
     fun removePlayer(playerName: String) {
