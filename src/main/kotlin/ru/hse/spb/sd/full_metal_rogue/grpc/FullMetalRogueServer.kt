86464073fff1e611b6e94be4558dc3308eace3aa
sharkovadarya <sharkovadarya@gmail.com>
2019-06-03 15:32:54 +0300

    Added grpc server.

---
+++ src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/grpc/FullMetalRogueServer.kt
@@ -0,0 +1,49 @@
+package ru.hse.spb.sd.full_metal_rogue.grpc
+
+import io.grpc.Server
+import io.grpc.ServerBuilder
+import java.util.logging.Logger
+
+/**
+ * A sample gRPC server that serve the RouteGuide (see route_guide.proto) service.
+ */
+class FullMetalRogueServer(private val port: Int) {
+    private val server: Server = ServerBuilder.forPort(port).addService(FullMetalRogueService())
+        .build()
+
+    /** Start serving requests.  */
+    fun start() {
+        server.start()
+        logger.info("Server started, listening on $port")
+        Runtime.getRuntime().addShutdownHook(object : Thread() {
+            override fun run() {
+                System.err.println("*** shutting down gRPC server since JVM is shutting down")
+                this@FullMetalRogueServer.stop()
+                System.err.println("*** server shut down")
+            }
+        })
+    }
+
+    /** Stop serving requests and shutdown resources.  */
+    fun stop() {
+        server.shutdown()
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private fun blockUntilShutdown() {
+        server.awaitTermination()
+    }
+
+    companion object {
+        private val logger = Logger.getLogger(FullMetalRogueServer::class.java.name)
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val server = FullMetalRogueServer(10000)
+            server.start()
+            server.blockUntilShutdown()
+        }
+    }
+}
