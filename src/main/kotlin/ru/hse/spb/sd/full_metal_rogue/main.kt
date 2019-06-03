707b6de3ab024be70dfca5005c1366de1d15736b
sharkovadarya <sharkovadarya@gmail.com>
2019-06-03 16:07:36 +0300

    Minor fix (added type).

--- src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/main.kt
+++ src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/main.kt
@@ -1,12 +1,13 @@
 package ru.hse.spb.sd.full_metal_rogue
 
+import ru.hse.spb.sd.full_metal_rogue.controller.Controller
 import ru.hse.spb.sd.full_metal_rogue.controller.LocalController
 import ru.hse.spb.sd.full_metal_rogue.ui.GUI
 import javax.swing.JFrame
 
 object GameState {
     val gui = GUI()
-    var currentController = LocalController()
+    var currentController: Controller = LocalController()
 }
 
 fun main() {
