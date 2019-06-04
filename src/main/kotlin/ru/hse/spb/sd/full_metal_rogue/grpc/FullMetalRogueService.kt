45f10d627f67f6733a5831b2361620bddbcded72
Alexandr Fedorov <fedorov2602@mail.ru>
2019-06-04 15:37:24 +0300

    FulleMeralRogueService implementation

--- src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/grpc/FullMetalRogueService.kt
+++ src/main/kotlin/ru/hse/spb/sd/full_metal_rogue/grpc/FullMetalRogueService.kt
@@ -10,23 +10,24 @@ import ru.hse.spb.sd.full_metal_rogue.controller.SelectCommand
 import ru.hse.spb.sd.full_metal_rogue.logic.level.LevelGenerator
 import ru.hse.spb.sd.full_metal_rogue.logic.level.StandardLevelGenerator
 import ru.hse.spb.sd.full_metal_rogue.logic.map.Direction
-import ru.hse.spb.sd.full_metal_rogue.view.View
 import java.util.concurrent.ConcurrentHashMap
 
 class FullMetalRogueService(private val levelGenerator: LevelGenerator = StandardLevelGenerator()) : FullMetalRogueServerGrpc.FullMetalRogueServerImplBase() {
-    private val games = ConcurrentHashMap<String, Game>()
-    private val observers = ConcurrentHashMap<String, StreamObserver<Server.View>>()
+    private val sessions = ConcurrentHashMap<String, GameSession>()
 
     /**
      */
     override fun subscribeGame(
         request: Server.SubscribeGameRequest,
-        responseObserver: io.grpc.stub.StreamObserver<Server.View>
+        responseObserver: StreamObserver<Server.View>
     ) {
-        if (!games.contains(request.gameName)) {
+        if (!sessions.contains(request.gameName)) {
             responseObserver.onError(IllegalArgumentException("No game with such name"))
             return
         }
+
+        val session = sessions[request.gameName]!!
+        val observers = session.observers
 
         observers[request.playerName] = responseObserver
     }
@@ -35,14 +36,17 @@ class FullMetalRogueService(private val levelGenerator: LevelGenerator = Standar
      */
     override fun sendCommand(
         request: Server.Action,
-        responseObserver: io.grpc.stub.StreamObserver<Server.SendCommandResponse>
+        responseObserver: StreamObserver<Server.SendCommandResponse>
     ) {
-        if (!games.contains(request.gameName)) {
+        if (!sessions.contains(request.gameName)) {
             responseObserver.onError(IllegalArgumentException("No game with such name"))
             return
         }
 
-        val game = games.get(request.gameName)!!
+        val session = sessions[request.gameName]!!
+
+        val game = session.game
+        val observers = session.observers
 
         val updates = mutableListOf<Pair<String, Server.View>>()
 
@@ -52,8 +56,6 @@ class FullMetalRogueService(private val levelGenerator: LevelGenerator = Standar
 
             for (entry in observers.entries) {
                 val playerName = entry.key
-                val observer = entry.value
-
                 val view = game.getView(playerName)//.toJson()
                 val update = Server.View.newBuilder().setJson("TODO").build()
                 updates.add(playerName to update)
@@ -62,15 +64,18 @@ class FullMetalRogueService(private val levelGenerator: LevelGenerator = Standar
 
         responseObserver.onNext(Server.SendCommandResponse.newBuilder().build())
         responseObserver.onCompleted()
+
+        for (update in updates)
+            observers[update.first]!!.onNext(update.second)
     }
 
     /**
      */
     override fun getGameList(
         request: Server.GameListRequest,
-        responseObserver: io.grpc.stub.StreamObserver<Server.GameListResponse>
+        responseObserver: StreamObserver<Server.GameListResponse>
     ) {
-        val response = Server.GameListResponse.newBuilder().addAllGames(games.keys().toList()).build()
+        val response = Server.GameListResponse.newBuilder().addAllGames(sessions.keys().toList()).build()
         responseObserver.onNext(response)
         responseObserver.onCompleted()
     }
@@ -79,15 +84,15 @@ class FullMetalRogueService(private val levelGenerator: LevelGenerator = Standar
      */
     override fun createGame(
         request: Server.CreateGameRequest,
-        responseObserver: io.grpc.stub.StreamObserver<Server.CreateGameResponse>
+        responseObserver: StreamObserver<Server.CreateGameResponse>
     ) {
 
-        synchronized(games) { // synchronized is needed to make creation atomic
-            if (games.contains(request.gameName)) {
+        synchronized(sessions) { // synchronized is needed to make creation atomic
+            if (sessions.contains(request.gameName)) {
                 responseObserver.onError(IllegalArgumentException("There is already game with such name"))
                 return
             }
-            games[request.gameName] = Game(levelGenerator.generateLevel())
+            sessions[request.gameName] = GameSession(Game(levelGenerator.generateLevel()))
         }
         responseObserver.onNext(Server.CreateGameResponse.newBuilder().build())
         responseObserver.onCompleted()
@@ -101,5 +106,9 @@ class FullMetalRogueService(private val levelGenerator: LevelGenerator = Standar
         Server.Command.LEFT -> DirectionCommand(Direction.LEFT)
         Server.Command.RIGHT -> DirectionCommand(Direction.RIGHT)
         Server.Command.UNRECOGNIZED -> throw IllegalArgumentException("Command is not recognized")
+    }
+
+    class GameSession(val game: Game) {
+        val observers = ConcurrentHashMap<String, StreamObserver<Server.View>>()
     }
 }
